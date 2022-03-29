34e7321d076ecb33f26b73bdef9f462d33f86f79
Jose Pavon <RannerJP@gmail.com>
2022-03-29 00:56:38 -0700

    Makes the matrix and fills in 0s

--- Matrix.java
+++ Matrix.java
@@ -1,6 +1,8 @@
+import java.util.Random;
 public class Matrix {
-    
-    public void generateMatrix(int size){
+    Random rand = new Random();
+
+    public int[][] generateMatrix(int size){
         double logTwo;
         int newSize = size;
         do{
@@ -8,6 +10,24 @@ public class Matrix {
             newSize++;
         }while(Math.floor(logTwo) != logTwo);
         newSize--;
+        int[][] matrix = new int[newSize][newSize];
+        for(int rowNums =0; rowNums < size; rowNums++){
+            for(int columnNums = 0; columnNums < size; columnNums++){
+                matrix[rowNums][columnNums] = rand.nextInt(101);
+            }
+            for(int columnZeros = size; columnZeros < newSize; columnZeros++){
+                matrix[rowNums][columnZeros] = 0;
+            }
+        }
+        for(int rowZeros =0; rowZeros < size; rowZeros++){
+            for(int columnNums = 0; columnNums < size; columnNums++){
+                matrix[rowZeros][columnNums] = rand.nextInt(100);
+            }
+            for(int columnZeros = size; columnZeros < size; columnZeros++){
+                matrix[rowZeros][columnZeros] = 0;
+            }
+        }
+        return matrix;
     }
 
     
@@ -28,5 +48,24 @@ public class Matrix {
     }
     public void strassensHelper(){
     
+    }
+
+    /**
+     * This method will display the matrix given it's size and 
+     * @param size is the number of equations in the matrix
+     * @param matrix is the array that holds our matrix
+     */
+    public static void displayMatrix(int size, int[][] matrix){
+        for(int i = 0; i < size; i++){
+            String row = "[ ";
+            for(int j = 0; j < size; j++){
+                if(j == size){
+                    row = row + ": ";
+                }
+                row = row + matrix[i][j] + " ";
+            }
+            row = row + "]";
+            System.out.println(row);
+        }
     }
 }
