dd3f3c224bbe478ae3c0fca4a2015bf8d5f5b737
“Joshua <joshuaereyes@cpp.edu>
2022-03-30 16:19:21 -0700

    start

--- Matrix.java
+++ Matrix.java
@@ -6,7 +6,16 @@ public class Matrix {
 
     
     public void bruteForceSolve(){
-
+        int multMatrix[][] = new int[row1][col2];
+        for(int i = 0; i < row1.length; i++){
+            for(int j = 0; j < column2.length; j++{
+                matrix[i][j] = 0;
+                for(int k = 0; k < row2.length; k++){
+                    multMatrix[i][j] += A[i][k] * B[k][j]
+                }
+            }
+            
+        }
     }
 
     public void divideAndConquer(){
